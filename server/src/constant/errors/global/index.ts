54a13595b8d9723cacf745378b6ed44d78001ece
ulises-jeremias <ulisescf.24@gmail.com>
2020-03-12 03:28:02 -0300

    Added error constants

---
+++ server/src/constant/errors/global/index.ts
@@ -0,0 +1,5 @@
+export * from './bad-request';
+export * from './internal-error';
+export * from './not-found';
+export * from './unprocessable-entity';
+export * from './base-error';
---
+++ server/src/constant/errors/global/internal-error.ts
@@ -0,0 +1,11 @@
+import { BaseError } from './base-error';
+import { constants } from 'http2';
+
+export class InternalError extends BaseError {
+    constructor() {
+        super();
+        this.statusCode = constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+        this.code = 'INTERNAL_ERROR';
+        this.message = 'The server encountered an internal error.';
+    }
+}
