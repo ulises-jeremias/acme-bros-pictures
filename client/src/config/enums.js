661ac4e12ed4a4370d43437179c12898eb2f8487
ulises-jeremias <ulisescf.24@gmail.com>
2020-03-19 05:56:06 -0300

    Updated enums

---
+++ client/src/config/enums.js
@@ -0,0 +1,23 @@
+export const TODO = 'todo';
+export const RUNNING = 'running';
+export const SUCCESS = 'success';
+export const FAILED = 'failed';
+
+export const STATUS_COLORS = {
+  [TODO]: 'grey',
+  [RUNNING]: 'blue',
+  [SUCCESS]: 'green',
+  [FAILED]: 'red',
+};
+
+export const STATUS_ICONS = {
+  [TODO]: 'clock outline',
+  [RUNNING]: 'circle notched',
+  [SUCCESS]: 'check',
+  [FAILED]: 'warning sign',
+};
+
+export const NOT_WATCHING = 'not-watching';
+export const RELEASES_ONLY = 'releases-only';
+export const WATCHING = 'watching';
+export const IGNORING = 'ignoring';
